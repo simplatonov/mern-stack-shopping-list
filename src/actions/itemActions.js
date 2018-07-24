56b29c0d7217431fff8a890b2bd1ca230c99da27
platonovsimeon <simeon.platonov@gmail.com>
2018-07-24 18:50:24 +0300

    connected to heroku

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -2,7 +2,7 @@ import {GET_ITEMS,ADD_ITEM,DELETE_ITEM,ITEMS_LOADING} from "./types";
 import axios from "axios";
 export const getItems = ()=> dispatch => {
     dispatch(setItemsLoading());
-    axios.get("http://localhost:5000/api/items").then(response=>{
+    axios.get("https://arcane-refuge-92393.herokuapp.com/api/items").then(response=>{
       dispatch({type:GET_ITEMS,payload:response.data});
       console.log(response);
     });
@@ -10,10 +10,10 @@ export const getItems = ()=> dispatch => {
 
 }
 export const deleteItem= (id)=> dispatch => {
-  axios.delete(`http://localhost:5000/api/items/${id}`).then(response=>dispatch({type:DELETE_ITEM,payload:id}));
+  axios.delete(`https://arcane-refuge-92393.herokuapp.com/api/items/${id}`).then(response=>dispatch({type:DELETE_ITEM,payload:id}));
 }
 export const addItem= (item)=> dispatch => {
-  axios.post("http://localhost:5000/api/items",item).then(response=>dispatch({type:ADD_ITEM,payload:response.data}));
+  axios.post("https://arcane-refuge-92393.herokuapp.com/api/items",item).then(response=>dispatch({type:ADD_ITEM,payload:response.data}));
 
 
 }
